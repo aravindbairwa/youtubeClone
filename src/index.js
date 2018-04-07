9db9530bc3b5c3881153e35ea3dbd7d7cf6fe604
aravindbairwa <34308465+aravindbairwa@users.noreply.github.com>
2018-04-07 13:43:16 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar.js'
+import VideoList from './components/video_list'
 // create a new componet. This component should produce some html
 const API_KEY = 'AIzaSyALJQuAwBHcjIdXW9YvRXbSQumkVNjcwPY'
 
-YTSearch({key:API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
 
 
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key:API_KEY, term:'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
